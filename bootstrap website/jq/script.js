33aef8fe1c04993e62f331c9b06e3732f2347854
patelpriya0987 <145538537+patelpriya0987@users.noreply.github.com>
2024-04-25 11:55:11 +0530

    Add files via upload

---
+++ bootstrap website/jq/script.js
@@ -0,0 +1,16 @@
+$( function() {
+    // init plugin
+    NodeCursor({
+      cursor : true, 
+      node : true, 
+      cursor_velocity : 1, 
+      node_velocity : 0.15, 
+      native_cursor : 'none', 
+      element_to_hover : '.nodeHover', 
+      cursor_class_hover : 'disable', 
+      node_class_hover : 'expand', 
+      hide_mode : true, 
+      hide_timing : 2000, 
+    });
+  
+  });
